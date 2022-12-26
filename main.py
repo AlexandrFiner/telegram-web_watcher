808f5f13c1a83011fb602abb312d1c7d798da590
79824 <matiahv@yandex.ru>
2022-12-26 21:59:03 +0500

    работа с телеграмм

--- main.py
+++ main.py
@@ -2,20 +2,44 @@ from gpiozero import MotionSensor
 import cv2
 import telebot
 import time
+from threading import Thread
+
+user_list = []
+
+token = '5814884989:AAGzD3cRDpIc1bJhKMBFMuLSOmRPRSVSTKU'
+bot = telebot.TeleBot(token, parse_mode=None)
+
+
+@bot.message_handler(commands=['start'])
+def add_to_list(message):
+    if message.chat.id not in user_list:
+        bot.reply_to(message, "Теперь вы будете получать изображения")
+        user_list.append(message.chat.id)
+    else:
+        bot.reply_to(message, "!! Вы уже получаете уведомления")
+
+
+@bot.message_handler(commands=['stop'])
+def add_to_list(message):
+    if message.chat.id in user_list:
+        bot.reply_to(message, "Вы отключились от уведомлений")
+        user_list.remove(message.chat.id)
+    else:
+        bot.reply_to(message, "!! Вы еще не получаете уведомления")
 
 
 def TelegramSendFile(input_file):
     # Написать код отправки файла в Телеграмм: Матяж Владимир
-    pass
+    for chatId in user_list:
+        bot.send_photo(chatId, open(input_file, 'rb'))
 
 
 def main():
     path = "images/"
-    pir = MotionSensor(4)
+    # pir = MotionSensor(4)
     try:
         camera = cv2.VideoCapture(0)
         while True:
-            #
             current_Time = str(time.strftime("%Y%m%d-%H%M%S"))
 
             print("Motion detected! ", current_Time)
@@ -23,7 +47,10 @@ def main():
                 camera.open(0)
 
             result, image = camera.read()
-            cv2.imwrite(path+current_Time+'.jpg', image)
+            # cv2.imwrite(path+current_Time+'.jpg', image)
+            image_path = path + 'image' + '.jpg'
+            cv2.imwrite(image_path, image)
+            TelegramSendFile(image_path)
             camera.release()
 
     except Exception as e:
@@ -31,4 +58,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    threadBot = Thread(target=bot.polling)
+    threadCamera = Thread(target=main)
+    threadBot.start()
+    threadCamera.start()
+    threadBot.join()
+    threadCamera.join()
